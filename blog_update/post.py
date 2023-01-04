edb8807c2fc8f2f61ac1a069621a2ccb5b15f60e
THSPSS <111225654+THSPSS@users.noreply.github.com>
2023-01-04 11:27:32 +0900

    Delete blog_update directory

--- blog_update/post.py
+++
@@ -1,9 +0,0 @@
-class Post:
-    def __init__(self, post_id, title, subtitle, body, author, dates, image):
-        self.id = post_id
-        self.title = title
-        self.subtitle = subtitle
-        self.body = body
-        self.author = author
-        self.dates = dates
-        self.image = image
